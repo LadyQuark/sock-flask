7e1523dacd78db295a9e96c0f5baca2d44803c5a
Dipika <dips@Dipikas-iMac.local>
2022-07-22 11:48:07 +0530

    Added header comment

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+""" 
+    Project to test logging user events using socketIO, mongoDB, Flask
+    User client connects to socketio
+    User clicks a button (blue/red)
+        triggering client to emit event to namespace '/log'
+        example of data received from client:
+        `{ 'event': 'button click', 'data': { name: 'Blue Button' } }`
+    Server saves event information to local mongoDB 
+        database 'eventLogDB', collection 'logs'
+            {
+                _id: ObjectId,
+                user: { type: ObjectId, ref: 'users' },
+                created: Date,
+                event: String,
+                data: Object,
+            }
+"""
+
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
@@ -9,15 +27,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 # configure PyMongo
-app.config["MONGO_URI"] = "mongodb://localhost:27017/eventLogDB"
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/eventLogDB'
 mongo = PyMongo(app)
 
 if __name__ == '__main__':
     socketio.run(app)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 # on connection
 @socketio.on('connect')
@@ -30,7 +48,7 @@ def disconnect():
     print('disconnect')
 
 # Log event in mongoDB
-@socketio.on("user event", namespace="/log")
+@socketio.on('user event', namespace='/log')
 def log_event(data):
     print(data)
     try:
@@ -41,7 +59,7 @@ def log_event(data):
             'data': data['data'],
         }
     except KeyError:
-        print("Insufficient data")
+        print('Insufficient data')
         return
     
     log_id = mongo.db.logs.insert_one(log).inserted_id
